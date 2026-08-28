f1de4e6d66f558e00a915888f6dacd8cbe8d6226
Ramadan <ramadanhamdan98@gmail.com>
2026-08-28 13:56:22 +0700

    perbaikan

--- src/app/api/tracking-summary/route.ts
+++ src/app/api/tracking-summary/route.ts
@@ -4,12 +4,12 @@ import clientPromise from "@/lib/mongodb"
 
 
 export async function GET(req: NextRequest) {
-    try {
-        const client = await clientPromise
-        const db = client.db('MabelHub')
-        const Broadcol = db.collection('tracking_broadcast')
-        const Callcol = db.collection('tracking_call')
-        const Valcol = db.collection('validasi_sales')
-        const Dbcol = db.collection('input_database')
-    }
+    // try {
+    //     const client = await clientPromise
+    //     const db = client.db('MabelHub')
+    //     const Broadcol = db.collection('tracking_broadcast')
+    //     const Callcol = db.collection('tracking_call')
+    //     const Valcol = db.collection('validasi_sales')
+    //     const Dbcol = db.collection('input_database')
+    // }
 }
